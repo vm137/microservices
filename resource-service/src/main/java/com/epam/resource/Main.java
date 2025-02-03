b0c170d1a39f074fb10742e69147232891a9dd56
Victor Mikhaylov <victor_mikhaylov@epam.com>
2025-02-03 13:00:58 +0400

    Initial

---
+++ resource-service/src/main/java/com/epam/resource/Main.java
@@ -0,0 +1,7 @@
+package com.epam.resource;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Resource service");
+    }
+}
---
+++ song-service/src/main/java/com/epam/song/Main.java
@@ -0,0 +1,7 @@
+package com.epam.song;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Song service");
+    }
+}
